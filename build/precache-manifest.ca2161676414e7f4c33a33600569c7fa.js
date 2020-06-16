d6d7b498364ec14a7f557ebbe6b18edd97c1c22f
saqib25 <52543072+saqib25@users.noreply.github.com>
2020-06-16 12:51:48 +0500

    added files

---
+++ build/precache-manifest.ca2161676414e7f4c33a33600569c7fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc56c6ea7ec53222711083892907dd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c7b336b9073b795fa45",
+    "url": "/static/css/main.00605e08.chunk.css"
+  },
+  {
+    "revision": "d7aade09ff26b7c6e9a2",
+    "url": "/static/js/2.afbdab6c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.afbdab6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c7b336b9073b795fa45",
+    "url": "/static/js/main.e38651f4.chunk.js"
+  },
+  {
+    "revision": "4e670287c3c8c5d2d70a",
+    "url": "/static/js/runtime-main.7d6e9700.js"
+  }
+]);
